a48144968478a761453b6d784c51138c0804f84d
GaiBang <3100247085@qq.com>
2017-02-05 21:51:34 +0800

    add rxpy

--- gloves-py/readme.txt
+++ gloves-py/readme.txt
@@ -1,2 +1,4 @@
 sudo pip install keyboard
 pip install pyserial
+
+pip install rx
---
+++ gloves-py/withrx.py
@@ -0,0 +1,20 @@
+import rx
+from rx import Observable, Observer
+
+class MyObserver(Observer):
+    def on_next(self, x):
+        print("Got: %s" % x)
+        
+    def on_error(self, e):
+        print("Got error: %s" % e)
+        
+    def on_completed(self):
+        print("Sequence completed")
+
+xs = Observable.from_iterable(range(10))
+d = xs.subscribe(MyObserver())
+
+xs = Observable.from_(range(10))
+d = xs.map(
+        lambda x: x * 2
+    ).subscribe(MyObserver())
