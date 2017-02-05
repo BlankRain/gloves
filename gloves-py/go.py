ce98590d21a51c492ea9e4cd8ada79eab06f51f7
GaiBang <3100247085@qq.com>
2017-02-05 21:41:55 +0800

    add something

--- gloves-py/go.py
+++ gloves-py/go.py
@@ -1,6 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
 import keyboard
-def cb(x):
+
+#@keyboard.hook
+def x(x):
     print(x.name)
+    
+def c():
+    keyboard.unhook()
+    
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/event/<eventname>')
+def action(eventname):   
+    return eventname
+        
+if __name__ == "__main__":
+    app.run()
 
 
-keyboard.hook(cb)
+
