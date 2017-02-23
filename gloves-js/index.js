21bf85c310a0776f1e217c3e774c27da3c7fdf1a
BlankRain <731014656@qq.com>
2017-02-23 16:15:09 +0800

    good job

--- gloves-js/index.js
+++ gloves-js/index.js
@@ -1,12 +1,30 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
- 
+var MQTT = require('mqtt');
+var EventEmitter = require('events').EventEmitter,
+    Rx = require('rx');
+var client  = MQTT.connect('mqtt://localhost')
+var eventEmitter = new EventEmitter();
+
 client.on('connect', function () {
   client.subscribe('glove/touch/event')
 })
- 
-client.on('message', function (topic, message) {
-  // message is Buffer 
-  console.log(message.toString())
-  client.end()
+/**
+ * 就是这么蛋疼,直接fromEvent 会掉参数~ 需要自己搞个eventEmitter提交一下~
+ */
+client.on('message',function(topic,message){
+    eventEmitter.emit('message', topic,message);
 })
+
+var source = Rx.Observable.fromEvent(
+    eventEmitter,
+    'message',
+    function (first, second) {
+        return { foo: first, bar: second };
+    });
+
+var subscription = source.subscribe(
+    function (x) {
+        console.log('Next: foo -' + x.foo + ', bar -' + x.bar);
+    },
+    function (err) { console.log('Error: ' + err); },
+    function () { console.log('Completed'); });
+
