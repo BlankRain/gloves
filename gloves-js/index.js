74d846678cc73bcd97ccafce244a0d390cbe7c7b
BlankRain <731014656@qq.com>
2017-02-23 15:51:38 +0800

    ok

--- gloves-js/index.js
+++ gloves-js/index.js
@@ -1,48 +1,12 @@
-var stompit = require('stompit');
-console.log("good Job")
-var connectOptions = {
-  'host': 'localhost',
-  'port': 1883
-  ,'connectHeaders':{
-    // 'host': '/',
-    // 'login': 'username',
-    // 'passcode': 'password',
-    // 'heart-beat': '5000,5000'
-  }
-};
-
-stompit.connect(connectOptions, function(error, client) {
-
-  if (error) {
-    console.log('connect error ' + error.message);
-    return;
-  }
-
-
-  var subscribeHeaders = {
-    'destination': 'glove/touch/event',
-    'ack': 'client-individual'
-  };
-
-  client.subscribe(subscribeHeaders, function(error, message) {
-
-    if (error) {
-      console.log('subscribe error ' + error.message);
-      return;
-    }
-
-    message.readString('utf-8', function(error, body) {
-
-      if (error) {
-        console.log('read message error ' + error.message);
-        return;
-      }
-
-      console.log('received message: ' + body);
-
-      client.ack(message);
-
-      client.disconnect();
-    });
-  });
-});
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+ 
+client.on('connect', function () {
+  client.subscribe('glove/touch/event')
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString())
+  client.end()
+})
