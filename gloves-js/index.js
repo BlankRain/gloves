179b66b9b498fbcf4cc7dfc454ff7b9c31a5d2e1
BlankRain <731014656@qq.com>
2017-02-23 16:25:48 +0800

    add event

--- gloves-js/index.js
+++ gloves-js/index.js
@@ -1,30 +1,27 @@
 var MQTT = require('mqtt');
-var EventEmitter = require('events').EventEmitter,
-    Rx = require('rx');
+var Rx = require('rx');
 var client  = MQTT.connect('mqtt://localhost')
-var eventEmitter = new EventEmitter();
 
 client.on('connect', function () {
   client.subscribe('glove/touch/event')
 })
-/**
- * 就是这么蛋疼,直接fromEvent 会掉参数~ 需要自己搞个eventEmitter提交一下~
- */
-client.on('message',function(topic,message){
-    eventEmitter.emit('message', topic,message);
-})
 
-var source = Rx.Observable.fromEvent(
-    eventEmitter,
-    'message',
-    function (first, second) {
-        return { foo: first, bar: second };
+
+var source=Rx.Observable
+    .fromEvent(client,'message',(t,m)=>{
+        return { topic: t, message: m };
     });
+    
+source.subscribe(
+    (x)=> {
+        console.log('Next: foo -' + x.topic + ', bar -' + x.message);
+        },
+    (err)=> { console.log('Error: ' + err); },
+    () =>{ console.log('Completed'); })
 
-var subscription = source.subscribe(
-    function (x) {
-        console.log('Next: foo -' + x.foo + ', bar -' + x.bar);
-    },
-    function (err) { console.log('Error: ' + err); },
-    function () { console.log('Completed'); });
-
+source.subscribe(
+    (x)=> {
+        console.log('OOO: foo -' + x.topic + ', bar -' + x.message);
+        },
+    (err)=> { console.log('Error: ' + err); },
+    () =>{ console.log('Completed'); })
