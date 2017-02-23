5fdbe83000f02743adbc2a6e1e7d3f28f0af0ca4
GaiBang <3100247085@qq.com>
2017-02-23 23:19:14 +0800

    add host port support

--- gloves-js/index.js
+++ gloves-js/index.js
@@ -1,20 +1,35 @@
-var MQTT = require('mqtt');
-var Rx = require('rx');
-var client  = MQTT.connect('mqtt://localhost')
 
-client.on('connect', function () {
-  client.subscribe('glove/touch/event')
-})
+const Rx = require('rx');
 
+function mqttSource(){
+    const MQTT = require('mqtt');
+    const client  = MQTT.connect('mqtt://localhost')
 
+    client.on('connect', ()=> {client.subscribe('glove/touch/event')})
+    return {source:client,onevent:"message",type:'mqtt'};
+}
+
+function hostPortSource(){
+    const net = require('net');
+    const readline = require('readline');
+    const localClient = net.connect({host:"192.168.44.135",port: 3002}, () => {});
+    const  rl = readline.createInterface({input:localClient, output:localClient});
+    return {source:rl,onevent:"line",type:'hostport'};
+}
+
+const watchSource=process.argv[2] !='mqtt'? hostPortSource():mqttSource();
 var source=Rx.Observable
-    .fromEvent(client,'message',(t,m)=>{
-        return { topic: t, message: m };
+    .fromEvent(watchSource['source'],watchSource['onevent'],(t,m)=>{
+        if(watchSource['type']=='mqtt'){
+            return { topic: t, message: m ,type:watchSource['type']};
+        }else{
+            return { topic: "glove/touch/event", message: t ,type:watchSource['type']};
+        }
     });
     
 source.subscribe(
     (x)=> {
-        console.log('Next: foo -' + x.topic + ', bar -' + x.message);
+        console.log('Topic Is:' + x.topic + ', Message Is:' + x.message);
         },
     (err)=> { console.log('Error: ' + err); },
     () =>{ console.log('Completed'); })
@@ -25,3 +40,4 @@ source.subscribe(
         },
     (err)=> { console.log('Error: ' + err); },
     () =>{ console.log('Completed'); })
+
