eb52192986853cab566c44be055cda102b4c4842
GaiBang <3100247085@qq.com>
2017-02-25 02:38:59 +0800

    add something

--- gloves-js/index.js
+++ gloves-js/index.js
@@ -26,18 +26,25 @@ var source=Rx.Observable
             return { topic: "glove/touch/event", message: t ,type:watchSource['type']};
         }
     });
-    
+
+const fiveGods ="紫薇我爱你"
+const PORT={2:0,3:1,4:2,5:3,6:4}
 source.subscribe(
     (x)=> {
-        console.log('Topic Is:' + x.topic + ', Message Is:' + x.message);
+        const m=x.message.split(",")
+        // console.log(m)  ==>[ '1', 'Continue', '801', '0', '418233', '6', 'Ni' ]
+        const whatIsay=fiveGods[PORT[m[5]]]
+        console.info(`${whatIsay} ${m[6]}`);
         },
     (err)=> { console.log('Error: ' + err); },
     () =>{ console.log('Completed'); })
 
 source.subscribe(
     (x)=> {
-        console.log('OOO: foo -' + x.topic + ', bar -' + x.message);
+        // console.log('OOO: foo -' + x.topic + ', bar -' + x.message);
         },
     (err)=> { console.log('Error: ' + err); },
     () =>{ console.log('Completed'); })
+
+
 
